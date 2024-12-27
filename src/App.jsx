2f2a1f9f7929515012a0037f33bb3fc3be3c835f
Shankerteja <vangurishankerteja@gmail.com>
2024-12-27 13:17:40 +0530

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { InfinitySpin } from 'react-loader-spinner'
 import './App.css'
 import { FaSearch } from "react-icons/fa";
+import toast, { Toaster } from 'react-hot-toast';
 const key=import.meta.env.VITE_API_KEY
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
   const getWeatherDetails=async ()=>{
 
  
-    const cityName=city ? city : "London"
+    const cityName=city ? city : "nagarkurnool"
 
     try {
       const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${key}`)
@@ -23,10 +24,14 @@ const App = () => {
         windSpeed:data.wind.speed,
         temperature:Math.floor(data.main.temp),
         location:data.name,
-        icon:data.weather[0].icon
+        icon:data.weather[0].icon,
+        situation:data.weather[0].description.toUpperCase(),
+        time :data.timezone
+      
       })
     } catch (error) {
       console.log(error)
+      toast.error('City Not Found!!');
       
     }
 
@@ -35,25 +40,35 @@ const App = () => {
   useEffect(()=>{
     getWeatherDetails()
   },[])
-
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    getWeatherDetails()
+  }
 
   const getCity=(event)=>{
     setCity(event.target.value)
 
   }
+
+  const currentUTC = new Date().getTime(); 
+  const localUTCOffset = new Date().getTimezoneOffset() * 60000; 
+  const utcTime = currentUTC + localUTCOffset;
+  const localTime = new Date(utcTime + weatherDetails.time * 1000);
+  const CurrentTime=localTime.toLocaleString()
   return (
     <div className='app-container'>
+        <Toaster />
    <h1 className='heading'>Weather App</h1>
    <div className='weather-card'>
-    <div className='search-container'>
-      <input type='text' placeholder='Search Your City eg:New York' onChange={getCity}/>
-      <button onClick={getWeatherDetails} ><FaSearch /></button>
-    </div>
+    <form className='search-container' onSubmit={handleSubmit}>
+      <input type='search' placeholder='Search Your City eg:Hyderabad' onChange={getCity} id='text' value={city}/>
+      <button type='submit' id='button'><FaSearch /></button>
+    </form>
    
-   {!weatherDetails ? <InfinitySpin
+   {weatherDetails.length==0 ? <InfinitySpin
   visible={true}
   width="200"
-  color="#4fa94d"
+  color="brown"
   ariaLabel="infinity-spin-loading"
   /> :
   <>
@@ -62,11 +77,16 @@ const App = () => {
    <h1 className='temperature'>
    {weatherDetails.temperature}&#8451;
    </h1>
+   <span className='situation'>{weatherDetails.situation}</span>
        <h1 className='city-name'>{weatherDetails.location}</h1>
+       <h3 className='time'>
+        Current Time :{CurrentTime}
+       </h3>
        <div className='other-details-container'>
    <div className='himidity-container'>
      <h4>Humidity</h4>
      <span>
+
 {weatherDetails.humidity}%
 </span>
    </div>
